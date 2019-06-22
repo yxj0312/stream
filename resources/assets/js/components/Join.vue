a59115602ff8d1fea97d08f82901822395e43e64
yxj0312 <yanxiajun@hotmail.com>
2019-06-22 22:26:56 +0200

    EP 03:16

---
+++ resources/assets/js/components/Join.vue
@@ -0,0 +1,27 @@
+<template>
+    <div slot="" class="mb-6">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [],
+
+        components: {},
+
+        data() {
+            return {
+                
+            }
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            
+        }
+    }
+</script>
