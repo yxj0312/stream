86dc84be10883428e758f9e757b943d7c52c55a1
yxj0312 <yanxiajun@hotmail.com>
2019-06-23 22:29:44 +0200

    Add

--- resources/assets/js/components/Join.vue
+++ resources/assets/js/components/Join.vue
@@ -1,6 +1,18 @@
 <template>
     <div slot="" class="mb-6">
-
+        <plan-cards
+            v-if="showPlans"
+            :initial-category="category"
+            plan-classes="mt-8"
+            @change-category="category = $event"
+        >
+            <template v-slot:heading="{category}">
+                <div slot="heading" class="mb-8">
+                    <h1 class="text-3xl font-light tracking-tight mb-8 ">
+                        {{ category === 'personalPlans' ? "Let's Get You Signed Up" : "Knowledge Grows in Numbers" }}
+                    </h1>
+                </div>
+            </template>
     </div>
 </template>
 
